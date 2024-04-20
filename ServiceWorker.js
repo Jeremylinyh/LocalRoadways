ef984dd72d7ea6c671aba252dc4388e38fc401bf
Jeremylinyh <160819565+Jeremylinyh@users.noreply.github.com>
2024-04-20 15:37:19 -0700

    dropboxed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-Local no traffic-0.1.0";
 const contentToCache = [
-    "Build/LocalRoadways.loader.js",
-    "Build/LocalRoadways.framework.js.unityweb",
-    "Build/LocalRoadways.data.unityweb",
-    "Build/LocalRoadways.wasm.unityweb",
+    "https://dl.dropboxusercontent.com/s/ii07kequ8rm4thy46ok6l/Local.loader.js?rlkey=hudp1gjv7srj3u9pb9qljp4z5&",
+    "https://dl.dropboxusercontent.com/s/k6m61lf8i6cqgrvg75pc1/Local.framework.js.unityweb?rlkey=ejtekqx5c9a5qzl9n012hws2i&",
+    "https://dl.dropboxusercontent.com/s/nfriirfnshe9xfpagxk24/Local.data.unityweb?rlkey=9ml5jitdx0edf32w06c14on4b&",
+    "https://dl.dropboxusercontent.com/s/onil2xfz66jw3v3ejgdzb/Local.wasm.unityweb?rlkey=60x9ttdq7pwxadze1tlmr1utu&",
     "TemplateData/style.css"
 
 ];
